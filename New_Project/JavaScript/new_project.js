628c23faeb8e7a66a6491e4404872f72bf968907
Username <usernumber232@gmail.com>
2018-10-10 18:37:28 +0000

    Fixed title sizes.

--- New_Project/JavaScript/new_project.js
+++ New_Project/JavaScript/new_project.js
@@ -1,7 +1,3 @@
-/*$(document).ready(() => {
-	$('#about').on('mouseenter', () => {
-	  $('#about-description').slideDown();
-	}).on('mouseleave', () => {
-	  $('#about-description').slideUp();
-	});
-});*/
+$(document).ready(() => {
+	
+});
