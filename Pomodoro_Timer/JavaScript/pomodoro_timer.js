da65e5f31ddecbce41a63831073698362ec944ed
Username <usernumber232@gmail.com>
2019-01-20 23:26:14 +0000

    Implemented format.

--- Pomodoro_Timer/JavaScript/pomodoro_timer.js
+++ Pomodoro_Timer/JavaScript/pomodoro_timer.js
@@ -1,3 +1,13 @@
 $(document).ready(() => {
+    $("#about").on("mouseenter", () => {
+	  $("#about-description").slideDown();
+	}).on("mouseleave", () => {
+	  $("#about-description").slideUp();
+	});
 	
+	$("#instructions").on("mouseenter", () => {
+	  $("#instructions-description").slideDown();
+	}).on("mouseleave", () => {
+	  $("#instructions-description").slideUp();
+	});	
 });
