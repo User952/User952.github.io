0256cc4507456d9b6142ecb1408f0f2bc40cc0e0
Username <usernumber232@gmail.com>
2019-01-22 23:44:01 +0000

    Minor changes.

--- Pomodoro_Timer/JavaScript/pomodoro_timer.js
+++ Pomodoro_Timer/JavaScript/pomodoro_timer.js
@@ -1,13 +1,56 @@
-$(document).ready(() => {
-    $("#about").on("mouseenter", () => {
-	  $("#about-description").slideDown();
-	}).on("mouseleave", () => {
-	  $("#about-description").slideUp();
+$(function() {
+	$('#about').on('click', function() {
+	  $('#about-description').slideDown();
+	}).on('mouseleave', function() {
+	  $('#about-description').slideUp();
 	});
 	
-	$("#instructions").on("mouseenter", () => {
-	  $("#instructions-description").slideDown();
-	}).on("mouseleave", () => {
-	  $("#instructions-description").slideUp();
-	});	
+	$('#instructions').on('click', function() {
+	  $('#instructions-description').slideDown();
+	}).on('mouseleave', function() {
+	  $('#instructions-description').slideUp();
+	});
+	
+	$('#about').on('mouseenter', function() {
+	  $(this).css('color', 'red');
+	}).on('mouseleave', function() {
+	  $(this).css('color', 'white');
+	});
+	
+	$('#instructions').on('mouseenter', function() {
+	  $(this).css('color', 'red');
+	}).on('mouseleave', function() {
+	  $(this).css('color', 'white');
+	});
+	
+	DATE METHODS
+	(When a Date object is created, a number of methods make it possible to perform
+	    operations on it.)
+	(https://api.sololearn.com/DownloadFile?id=2772)
+	--------------------
+	var d = new Date();
+	var hours = d.getHours();
+	//hours is equal to the current hour
+	--------------------
+	--------------------
+	(Let's create a program that prints the current time to the browser once every
+	    second.)
+	
+	function printTime() {
+	  var d = new Date();
+	  var hours = d.getHours();
+	  var mins = d.getMinutes();
+	  var secs = d.getSeconds();
+	  document.body.innerHTML = hours+':'+mins+':'+secs;
+	}
+	setInterval(printTime, 1000);
+	
+	(We declared a function printTime(), which gets the current time from the date
+	    object, and prints it to the screen. We then called the function once every
+	    second, using the setInterval method.)
+	--------------------
+	(The innerHTML property sets or returns the HTML content of an element. In our
+	    case, we are changing the HTML content of our document's body. This
+	    overwrites the content every second, instead of printing it repeatedly to
+    	the screen.)
 });
