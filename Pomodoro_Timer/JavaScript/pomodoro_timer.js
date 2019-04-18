769046f9100baeb20e72d5fda5e6aff9c74d0d8a
Username <usernumber232@gmail.com>
2019-04-18 21:49:12 +0000

    Button click CSS.

--- Pomodoro_Timer/JavaScript/pomodoro_timer.js
+++ Pomodoro_Timer/JavaScript/pomodoro_timer.js
@@ -4,16 +4,63 @@ function move () {
 	
 }
 
+/*
+$().on('click', function() {
+	
+});
+// Show thumbnails within the table.
+function showImage(cell) {
+	var img = cell.getElementsByTagName("img")[0];
+	img.style.visibility = "visible";
+}
+
+function removeImage(cell) {
+	var img = cell.getElementsByTagName("img")[0];
+	img.style.visibility = "hidden";
+}
+
+var slideshowCounter = 0;
+var slideshowPictures = ["http://campusmaps.umn.edu/sites/campusmaps.umn.edu/files/styles/building_photos/public/akerman_150817_8599.jpg?itok=L9cER5AS",
+"http://campusmaps.umn.edu/sites/campusmaps.umn.edu/files/styles/building_photos/public/amundson_150626_4265.jpg?itok=LUix4YKx",
+"http://campusmaps.umn.edu/sites/campusmaps.umn.edu/files/styles/building_photos/public/kellerhall_150626_4282.jpg?itok=qcveFOWw"];
+var player;
+
+// Show a slideshow when the appropriate button is clicked.
+function slideshow() {
+	var view = document.getElementById("view");
+	view.setAttribute("src", slideshowPictures[slideshowCounter++]);
+	
+	if (slideshowCounter >= slideshowPictures.length) {
+		slideshowCounter = 0;
+	}
+}
+
+function startSlideshow() {
+	player = setInterval(function() {slideshow()}, 1000);
+}
+
+function stopSlideshow() {
+	player = clearInterval(player);
+}
+*/
+
 $(function() {
 	$('#about').on('click', function() {
 		$('#instructions-description').slideUp();
-	  $('#about-description').slideToggle();
+		$('#about-description').slideToggle();
 	});
 	
 	$('#instructions').on('click', function() {
 		$('#about-description').slideUp();
-	  $('#instructions-description').slideToggle();
+		$('#instructions-description').slideToggle();
 	});
+	
+	
+	
+	/*$('.button').on('click', function(event) {
+		$(event.currentTarget).addClass('activeButton');
+    $(event.currentTarget).siblings().removeClass('activeButton');
+	});*/
 	
 	/*
 	DATE METHODS
